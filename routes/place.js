7c6242548173b75a50ca900a5347b504ea452b0a
Andres Reyes <oreyes@cads.com.gt>
2018-09-13 12:15:56 -0600

    :construction: Se creo los servicios de los lugares

--- routes/place.js
+++ routes/place.js
@@ -12,7 +12,7 @@ app.post('/', (req, res) => {
 
   place.save((err, placeSaved) => {
     console.log(placeSaved);
-    if(err){
+    if (err) {
       return res.status(400).json({
         ok: false,
         message: 'Error al crear el lugar',
@@ -23,6 +23,71 @@ app.post('/', (req, res) => {
       ok: true,
       place: placeSaved
     });
+  });
+});
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Place.findById(id)
+    .exec((err, place) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          message: 'Error al buscar lugar',
+          errors: err
+        });
+      }
+      if (!place) {
+        return res.status(400).json({
+          ok: false,
+          message: 'El lugar con el id ' + id + ' no existe',
+          errors: {
+            message: 'El lugar con el id ' + id + ' no existe'
+          }
+        });
+      }
+      res.status(200).json({
+        ok: true,
+        place: place
+      })
+    });
+});
+
+app.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  Place.findById(id, (err, place) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al actualizar el lugar',
+        errors: err
+      });
+    }
+    if (!place) {
+      return res.status(400).json({
+        ok: false,
+        message: 'El lugar con el id ' + id + ' no existe',
+        errors: {
+          message: 'El lugar con el id ' + id + ' no existe'
+        }
+      })
+    }
+    place.name = body.name;
+    place.state = body.state;
+    place.save((err, place) => {
+      if (err) {
+        return res.status(500).json({
+          ok:false,
+          message: 'Error al actualizar el lugar',
+          errors: err
+        });
+      }
+      res.status(200).json({
+        ok: true,
+        place: place
+      });
+    })
   });
 });
 
