1db0b6e2f61ba74503cb9f2d30316ed07b6525e6
Andres Reyes <oreyes@cads.com.gt>
2018-09-21 16:24:14 -0600

    :construction: Se creo servicio de de listas

--- routes/list.js
+++ routes/list.js
@@ -26,4 +26,35 @@ app.post('/', mdAutentication.verifyToken, (req, res) => {
     });
   })
 });
+
+app.get('/state/:finished/:idPlace', mdAutentication.verifyToken, (req, res) => {
+  const finished = req.params.finished;
+  const idPlace = req.params.idPlace;
+  List.find({ finished: finished, idPlace: idPlace})
+  .select('_id name')
+    .exec((err, lists) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          message: 'Error al buscar las listas',
+          errors: {
+            message: 'Error al buscar las listas'
+          }
+        });
+      }
+      if (!lists) {
+        return res.status(400).json({
+          ok: false,
+          message: 'No tiene listas creadas',
+          errors: {
+            message: 'No tiene listas creadas'
+          }
+        })
+      }
+      res.status(200).json({
+        ok: true,
+        lists: lists
+      });
+    });
+});
 module.exports = app;
