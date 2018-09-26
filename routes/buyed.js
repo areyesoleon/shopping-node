da5592ff07f32f7fe632febe2158061dd938d1e3
Andres Reyes <oreyes@cads.com.gt>
2018-09-26 12:35:55 -0600

    :construction: servicio para cerrar las listas

--- routes/buyed.js
+++ routes/buyed.js
@@ -46,4 +46,86 @@ app.put('/:listId', mdAutentication.verifyToken, (req, res) => {
   });
 });
 
+app.put('/list/:listId/finished/:finished', mdAutentication.verifyToken, (req, res) => {
+  const listId = req.params.listId;
+  const body = req.body;
+  List.findById(listId, (err, list) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al actualizar la lista',
+        errors: err
+      });
+    }
+    if (!list) {
+      return res.status(400).json({
+        ok: false,
+        message: 'la lista con el id ' + listId + ' no existe',
+        errors: {
+          message: 'la lista con el id ' + listId + ' no existe'
+        }
+      });
+    }
+    list.itemList.forEach((item) => {
+      if (item.itemId === body.itemId) {
+        item.buyed = !item.buyed;
+      }
+    });
+    list.save((err, list) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          message: 'Error al acutalizar el producto',
+          errors: {
+            message: 'Error al acutalizar el producto',
+          }
+        });
+      }
+      res.status(200).json({
+        ok: true,
+        list: list
+      });
+    });
+  });
+});
+app.put('/listshop/:listId', mdAutentication.verifyToken, (req, res) => {
+  const listId = req.params.listId;
+  console.log(listId);
+  List.findById(listId, (err, list) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al actualizar la lista',
+        errors: err
+      });
+    }
+    if (!list) {
+      return res.status(400).json({
+        ok: false,
+        message: 'la lista con el id ' + listId + ' no existe',
+        errors: {
+          message: 'la lista con el id ' + listId + ' no existe'
+        }
+      });
+    }
+    list.finished = true;
+    list.save((err, list) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          message: 'Error al acutalizar el producto',
+          errors: {
+            message: 'Error al acutalizar el producto',
+          }
+        });
+      }
+      res.status(200).json({
+        ok: true,
+        list: list
+      });
+    });
+  });
+});
+
+
 module.exports = app;
